48faba9a4894a239cc123aedb68de9533b9b3a6f
Arjav Jain <arjavjain0703@gmail.com>
2025-10-20 22:48:33 +0530

    add ratatui template

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,34 @@
+use color_eyre::Result;
+use crossterm::event::{self, Event};
+use ratatui::{DefaultTerminal, Frame};
+
 mod structures;
 use structures::Directory;
 
 #[tokio::main]
-async fn main() {
-    println!("Hello, world!");
+async fn main() -> Result<()> {
+    color_eyre::install()?;
+    let terminal = ratatui::init();
+    let result = run(terminal);
+    ratatui::restore();
 
     let mut current_dir = Directory::new("tui-file-manager".to_string(), ".".to_string());
 
     current_dir.scan_and_add().await.unwrap();
     dbg!(current_dir);
+
+    result
+}
+
+fn run(mut terminal: DefaultTerminal) -> Result<()> {
+    loop {
+        terminal.draw(render)?;
+        if matches!(event::read()?, Event::Key(_)) {
+            break Ok(());
+        }
+    }
+}
+
+fn render(frame: &mut Frame) {
+    frame.render_widget("hello world", frame.area());
 }
