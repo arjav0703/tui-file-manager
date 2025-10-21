8f964ed7ac8c1e375691af90a1acd9564ff5f87f
Arjav Jain <arjavjain0703@gmail.com>
2025-10-21 20:38:50 +0530

    move the app struct to app.rs

---
+++ src/app.rs
@@ -0,0 +1,60 @@
+use color_eyre::Result;
+use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind, KeyModifiers};
+use ratatui::{
+    DefaultTerminal, Frame,
+    style::Stylize,
+    text::Line,
+    widgets::{Block, Paragraph},
+};
+
+#[derive(Debug)]
+pub struct App {
+    pub exit: bool,
+}
+
+impl App {
+    pub fn run(mut self, mut terminal: DefaultTerminal) -> Result<()> {
+        while !self.exit {
+            terminal.draw(|frame| self.render(frame))?;
+            self.handle_crossterm_events()?;
+        }
+        Ok(())
+    }
+
+    fn render(&mut self, frame: &mut Frame) {
+        let title = Line::from("Ratatui Simple Template")
+            .bold()
+            .blue()
+            .centered();
+        let text = "Hello, Ratatui!\n\n\
+            Created using https://github.com/ratatui/templates\n\
+            Press `Esc`, `Ctrl-C` or `q` to stop running.";
+        frame.render_widget(
+            Paragraph::new(text)
+                .block(Block::bordered().title(title))
+                .centered(),
+            frame.area(),
+        )
+    }
+
+    fn handle_crossterm_events(&mut self) -> Result<()> {
+        match event::read()? {
+            // it's important to check KeyEventKind::Press to avoid handling key release events
+            Event::Key(key) if key.kind == KeyEventKind::Press => self.on_key_event(key),
+            Event::Mouse(_) => {}
+            Event::Resize(_, _) => {}
+            _ => {}
+        }
+        Ok(())
+    }
+
+    fn on_key_event(&mut self, key: KeyEvent) {
+        if let (_, KeyCode::Esc | KeyCode::Char('q')) = (key.modifiers, key.code) {
+            self.quit();
+        }
+    }
+
+    fn quit(&mut self) {
+        self.exit = true;
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,9 @@
 use color_eyre::Result;
-use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind, KeyModifiers};
-use ratatui::{
-    DefaultTerminal, Frame,
-    style::Stylize,
-    text::Line,
-    widgets::{Block, Paragraph},
-};
 
 mod structures;
 use structures::Directory;
+mod app;
+use app::App;
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -25,56 +20,4 @@ async fn main() -> Result<()> {
     dbg!(entries);
 
     result
-}
-
-#[derive(Debug)]
-pub struct App {
-    pub exit: bool,
-}
-
-impl App {
-    pub fn run(mut self, mut terminal: DefaultTerminal) -> Result<()> {
-        while !self.exit {
-            terminal.draw(|frame| self.render(frame))?;
-            self.handle_crossterm_events()?;
-        }
-        Ok(())
-    }
-
-    fn render(&mut self, frame: &mut Frame) {
-        let title = Line::from("Ratatui Simple Template")
-            .bold()
-            .blue()
-            .centered();
-        let text = "Hello, Ratatui!\n\n\
-            Created using https://github.com/ratatui/templates\n\
-            Press `Esc`, `Ctrl-C` or `q` to stop running.";
-        frame.render_widget(
-            Paragraph::new(text)
-                .block(Block::bordered().title(title))
-                .centered(),
-            frame.area(),
-        )
-    }
-
-    fn handle_crossterm_events(&mut self) -> Result<()> {
-        match event::read()? {
-            // it's important to check KeyEventKind::Press to avoid handling key release events
-            Event::Key(key) if key.kind == KeyEventKind::Press => self.on_key_event(key),
-            Event::Mouse(_) => {}
-            Event::Resize(_, _) => {}
-            _ => {}
-        }
-        Ok(())
-    }
-
-    fn on_key_event(&mut self, key: KeyEvent) {
-        if let (_, KeyCode::Esc | KeyCode::Char('q')) = (key.modifiers, key.code) {
-            self.quit();
-        }
-    }
-
-    fn quit(&mut self) {
-        self.exit = true;
-    }
 }
