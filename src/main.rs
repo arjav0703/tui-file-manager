da20f9d338e72f6b155fe9da2aa7a3d8cc81bc09
Arjav Jain <arjavjain0703@gmail.com>
2025-10-21 20:35:51 +0530

    use the ratatui template

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
-use std::time::Duration;
-
 use color_eyre::Result;
-use crossterm::event::{self, Event};
-use ratatui::{DefaultTerminal, Frame};
+use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind, KeyModifiers};
+use ratatui::{
+    DefaultTerminal, Frame,
+    style::Stylize,
+    text::Line,
+    widgets::{Block, Paragraph},
+};
 
 mod structures;
 use structures::Directory;
@@ -10,9 +13,9 @@ use structures::Directory;
 #[tokio::main]
 async fn main() -> Result<()> {
     color_eyre::install()?;
-    let mut terminal = ratatui::init();
-    let mut app = App { exit: false };
-    let result = app.run(&mut terminal);
+    let terminal = ratatui::init();
+    let app = App { exit: false };
+    let result = app.run(terminal);
     ratatui::restore();
 
     let mut current_dir = Directory::new("tui-file-manager".to_string(), ".".to_string());
@@ -24,29 +27,54 @@ async fn main() -> Result<()> {
     result
 }
 
+#[derive(Debug)]
 pub struct App {
     pub exit: bool,
 }
 
 impl App {
-    fn run(&mut self, terminal: &mut DefaultTerminal) -> Result<()> {
-        use crossterm::event;
-        use std::time::Duration;
-
+    pub fn run(mut self, mut terminal: DefaultTerminal) -> Result<()> {
         while !self.exit {
-            let _ = terminal.draw(|frame| self.render(frame));
-
-            if event::poll(Duration::from_millis(100))?
-                && let Event::Key(key) = event::read()?
-                && let event::KeyCode::Char('q') = key.code
-            {
-                self.exit = true;
-            }
+            terminal.draw(|frame| self.render(frame))?;
+            self.handle_crossterm_events()?;
         }
         Ok(())
     }
 
-    fn render(&self, frame: &mut Frame) {
-        frame.render_widget("hello world", frame.area());
+    fn render(&mut self, frame: &mut Frame) {
+        let title = Line::from("Ratatui Simple Template")
+            .bold()
+            .blue()
+            .centered();
+        let text = "Hello, Ratatui!\n\n\
+            Created using https://github.com/ratatui/templates\n\
+            Press `Esc`, `Ctrl-C` or `q` to stop running.";
+        frame.render_widget(
+            Paragraph::new(text)
+                .block(Block::bordered().title(title))
+                .centered(),
+            frame.area(),
+        )
+    }
+
+    fn handle_crossterm_events(&mut self) -> Result<()> {
+        match event::read()? {
+            // it's important to check KeyEventKind::Press to avoid handling key release events
+            Event::Key(key) if key.kind == KeyEventKind::Press => self.on_key_event(key),
+            Event::Mouse(_) => {}
+            Event::Resize(_, _) => {}
+            _ => {}
+        }
+        Ok(())
+    }
+
+    fn on_key_event(&mut self, key: KeyEvent) {
+        if let (_, KeyCode::Esc | KeyCode::Char('q')) = (key.modifiers, key.code) {
+            self.quit();
+        }
+    }
+
+    fn quit(&mut self) {
+        self.exit = true;
     }
 }
